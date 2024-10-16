6c8d98c26141d712e9713deb08ea04d67ce5b0ef
iahmed <iahmed@stellaechnology.com>
2024-10-16 13:50:24 +0500

    places api

--- src/app/modules/common/location-filter/location-filter.component.ts
+++ src/app/modules/common/location-filter/location-filter.component.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/googlemaps" />
 import { Component, OnInit } from '@angular/core';
 import { GeolocationService } from '../../../../app/common/services/geolocation.service';
 import { PlacesService } from 'src/app/common/services/places.service';
@@ -15,23 +16,32 @@ export class LocationFilterComponent implements OnInit {
     private httpService: HttpService) { }
 
   ngOnInit(): void {
-    this.geolocationService.getPosition().subscribe((coords: any) => {
-      this.searchNearbyPlaces(coords.latitude, coords.longitude);
-    });
+    this.initNearbyPlacesService()
   }
 
-  searchNearbyPlaces(lat: number, lng: number) {
-    const radius = 1500; // Search within 1.5 km
-    this.placesService.getNearbyPlaces(lat, lng, radius).subscribe((data) => {
-      console.log(data.results)
+  initNearbyPlacesService(): void {
+    const service = new google.maps.places.PlacesService(document.createElement('div'));
+
+    const request = {
+      location: new google.maps.LatLng(33.6461432,73.0523224), // Replace with actual lat/lng
+      radius: 5000, // 5km radius
+      type: 'restaurant', // Only get restaurants
+    };
+
+    service.nearbySearch(request, (results: any, status: string) => {
+      if (status === 'OK' && results) {
+        results.forEach((place: any) => {
+          console.log(place.name); // List restaurant names in the console
+        });
+      }
     });
   }
 
   onKey(event: KeyboardEvent) {
     if (event.key === 'Enter') {
-      this.geolocationService.getPosition().subscribe((coords) => {
-        this.searchNearbyPlaces(coords.latitude, coords.longitude);
-      });
+      // this.geolocationService.getPosition().subscribe((coords) => {
+      //   this.searchNearbyPlaces(coords.latitude, coords.longitude);
+      // });
     }
   }
 
